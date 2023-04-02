1867b26de3e2776d68de70ba2529ccc44e187cc7
Gizem DİNÇSEVEN <125489450+gizemdncsvn@users.noreply.github.com>
2023-04-02 18:50:16 +0300

    Add files via upload

---
+++ SQL HW9/SQL HW9Q2.sql
@@ -0,0 +1,4 @@
+-- customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+
+SELECT payment_id, first_name, last_name FROM payment
+INNER JOIN customer ON customer.customer_id = payment.customer_id;
---
+++ SQL HW9/SQL HW9Q3.sql
@@ -0,0 +1,4 @@
+-- customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+
+SELECT rental_id, first_name, last_name FROM rental
+INNER JOIN customer ON customer.customer_id = rental.customer_id;
